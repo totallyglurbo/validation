3a62d780bb6fabc38644903b5e20feb8593f4ae3
totallyglurbo <glurb@glurbin.com>
2026-04-08 17:00:55 +0700

    Initial commit

---
+++ src/ValidatorHelper.php
@@ -0,0 +1,22 @@
+<?php
+namespace Validator;
+
+trait ValidatorHelper
+{
+    protected function matchPattern(string $pattern): bool
+    {
+        return (bool) preg_match($pattern, $this->value);
+    }
+
+    protected function isUnique(string $table, string $column): bool
+    {
+        return (bool)!Capsule::table($table)
+            ->where($column, $this->value)
+            ->count();
+    }
+
+    protected function isRequired(): bool
+    {
+        return !empty($this->value);
+    }
+}
